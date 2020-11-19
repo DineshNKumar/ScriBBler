7c3df2ab69c4dd572039fe2aa3b21beedfbdffff
DineshNKumar <dnkumar0510@gmail.com>
2020-11-19 12:51:43 -0800

    changes

--- bloglist/bloglist.js
+++ bloglist/bloglist.js
@@ -1,3 +1,13 @@
 function showPopUp(){
     document.getElementsByClassName("alert-box")[0].style.visibility = "visible" ;
 }
+
+// This will trigger then we will press on "Sign In Button"
+document.getElementById('buttons').children[0].addEventListener("click", function(){
+    document.getElementsByClassName("boxes")[0].style.visibility = "visible";
+});
+
+// This will trigger then we will press on "Sign Up Button"
+document.getElementById('buttons').children[1].addEventListener("click", function(){
+    document.getElementsByClassName("boxes")[1].style.visibility = "visible";
+});
