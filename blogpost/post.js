f0580bd635e36a90116f6da35510ce852dcfd2df
DineshNKumar <dnkumar0510@gmail.com>
2020-11-19 13:24:59 -0800

    Add post

--- blogpost/post.js
+++ blogpost/post.js
@@ -12,6 +12,12 @@ document.getElementById("text-area").addEventListener("focusout", function(){
     this.style.overflow = 'hidden';
 });
 
+document.getElementById("text-area").addEventListener("input", function(){
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+});
+
+
 // Like button
 var count = 1;
 document.getElementsByClassName("buttons")[1].addEventListener("click", function(){
@@ -32,4 +38,15 @@ document.getElementsByClassName("buttons")[2].addEventListener("click", function
     para.style.borderRadius = "10px";
     var list = document.getElementById("comments");
     list.insertBefore(para, list.childNodes[0]);
+});
+
+
+// This will trigger then we will press on "Sign In Button"
+document.getElementById('buttons').children[0].addEventListener("click", function(){
+    document.getElementsByClassName("boxes")[0].style.visibility = "visible";
+});
+
+// This will trigger then we will press on "Sign Up Button"
+document.getElementById('buttons').children[1].addEventListener("click", function(){
+    document.getElementsByClassName("boxes")[1].style.visibility = "visible";
 });
